7dc3648b9dd9d777d801b231031ed83b65c166c0
Sharix111 <sharshithab2021@gmail.com>
2026-07-01 10:19:21 +0530

    feat: remove deprecated authentication and user model files, streamline codebase

--- backend/src/controllers/auth.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-export declare const registerUser: (req: Request, res: Response) => Promise<void>;
-export declare const loginUser: (req: Request, res: Response) => Promise<void>;
-export declare const logoutUser: (req: Request, res: Response) => Promise<void>;
-export declare const refreshToken: (req: Request, res: Response) => Promise<void>;
-export declare const forgotPassword: (req: Request, res: Response) => Promise<void>;
-//# sourceMappingURL=auth.controller.d.ts.map
--- backend/src/middlewares/auth.middleware.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { IUser } from '../models/User';
-export interface AuthRequest extends Request {
-    user?: IUser;
-}
-export declare const protect: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-export declare const authorize: (...roles: string[]) => (req: Request, res: Response, next: NextFunction) => void;
-//# sourceMappingURL=auth.middleware.d.ts.map
